9181c7eb941afa2f1deb8d65af08964c9f9cebc2
ddozza54 <bananamayong@gmail.com>
2024-03-21 02:13:39 +0900

    ➕ Header Component 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-
+import { Link } from 'react-router-dom';
+import pepeImg from '../assets/pepe-movie.jpg'
 export default function Header() {
     return (
-        <div>
-            Header
+        <div className='bg-[#F0DFD5] w-full flex justify-between items-center'>
+            <div>
+                <img src={pepeImg} className='w-32' />
+                <span className='font-bold text-2xl text-green-700'>PEPEPLIX</span>
+            </div>
+            <nav className='font-bold w-[80%] '>
+                <ul className='w-full flex justify-around items-center'>
+                    <li>
+                        <Link to={'/'}>POPULAR</Link>
+                    </li>
+                    <li>
+                        <Link to={'/coming-soon'}>
+                            COMING SOON
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/now-playing'}>
+                            NOW PLAYING
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
